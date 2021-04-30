f93791eb69fe2ee078ed36394776fcf931b85821
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-04-30 05:52:31 -0500

    first phase

---
+++ part1/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+// console.log("staring to know")
+const now = new Date();
+const a = 15
+const b = 20
+
+
+const App = () => (
+
+  
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+)
+
+export default App
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
