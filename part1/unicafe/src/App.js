c0b011922f96880c575aca7cb6eb8d052e35d074
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-05-06 08:11:50 -0500

    Ended unicafe exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+const Title = ({text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{text}</h1>
+  )
 }
 
-export default App;
+const sumAllIntsInList = (listOfInt) => listOfInt.reduce((accumulator, current) => accumulator + current)
+
+const getAverage = (listOfCategories) => {
+  const numberOf1Cat = listOfCategories[0]
+  const numberOf2Cat = listOfCategories[1]
+  const numberOf3Cat = listOfCategories[2]
+
+  const valueOfCat1 = 1 
+  const valueOfCat2 = 0
+  const valueOfCat3 = -1
+
+  return ((numberOf1Cat * valueOfCat1) + (numberOf2Cat * valueOfCat2) + (numberOf3Cat * valueOfCat3)) / sumAllIntsInList(listOfCategories)
+
+}
+
+const LabelInfo = ({text, valueToShow}) => {
+  return (
+    <p>{text} {valueToShow}</p>
+  )
+}
+
+
+const ButtonIncreaseByOne = ({functionToCall, buttonText, valueToChange}) => {
+  // console.log(valueToChange)
+  return (
+    <button onClick={() => functionToCall(valueToChange + 1)}>{buttonText}</button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0) 
+  const listOfAllValues = [good, neutral, bad]
+
+  return (
+    <div>
+      <Title text="Give feedback"/>
+      <ButtonIncreaseByOne buttonText="good" functionToCall={setGood} valueToChange={good}/>
+      <ButtonIncreaseByOne buttonText="neutral" functionToCall={setNeutral} valueToChange={neutral}/>
+      <ButtonIncreaseByOne buttonText="bad" functionToCall={setBad} valueToChange={bad}/>
+      <Title text="Statistics"/>
+      <LabelInfo text="good" valueToShow={good}/>
+      <LabelInfo text="neutral" valueToShow={neutral}/>
+      <LabelInfo text="bad" valueToShow={bad}/>
+      <LabelInfo text="all" valueToShow={sumAllIntsInList(listOfAllValues)}/>
+      <LabelInfo text="average" valueToShow={getAverage(listOfAllValues)}/>
+      <LabelInfo text="positive" valueToShow={(good / sumAllIntsInList(listOfAllValues)) * 100 + "%"}/>
+    </div>
+  )
+}
+
+export default App
