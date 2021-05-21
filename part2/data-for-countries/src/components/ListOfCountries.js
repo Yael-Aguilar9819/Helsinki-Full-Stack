8121640534dbc45e212943ca655422bc568bde03
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-05-21 03:28:31 -0500

    Using useEffect to re-render list of countries, now refactorizing

--- part2/data-for-countries/src/components/ListOfCountries.js
+++ part2/data-for-countries/src/components/ListOfCountries.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CountryInList from "./CountryInList.js"
+import SingleCountryDescription from "./SingleCountryDescription.js"
 
+//This is the list of countries that will be rendered, with a button to re-render and make it a single country displayer
 const ListOfCountries = ({arrayOfCountries, functionSetText}) => {
+    const [showingSingleCountry, setshowingSingleCountry] = useState(false);
+    const [countryToShow, setCountryToShow] = useState("");
 
-    return (
-        <ul>
-            {arrayOfCountries.map(countryObject => {
-                return <CountryInList key={countryObject.name} countryObject={countryObject} functionSetText={functionSetText}/>
-            } 
-            )}
-        </ul>
-    )
+
+    const buttonStyle = {
+        marginLeft: 6,
+        maxHeight: 20
+    }
+    
+
+    useEffect(() => {
+        if (countryToShow === "") {
+
+        }
+        else {
+            setshowingSingleCountry(true);
+        }
+    })
+        // return <SingleCountryDescription countrySelected={countryObject}/>}
+    
+
+
+    if (showingSingleCountry === false) {
+        console.log(arrayOfCountries)
+        return (
+            <ul>
+                {arrayOfCountries.map(countryObject => {
+                // return <CountryInList key={countryObject.name} countryObject={countryObject} functionSetText={functionSetText}/>
+                return  <>
+                <li>{countryObject.name}
+                <button onClick={() => setCountryToShow(countryObject)} style={buttonStyle}>show</button>
+                </li>
+                </>
+                } 
+                )}
+            </ul>
+        )            
+    }
+
+    else {
+        console.log(countryToShow);
+        return (
+            <SingleCountryDescription countrySelected={countryToShow}/>
+        )
+    }
 }
 
 export default ListOfCountries
