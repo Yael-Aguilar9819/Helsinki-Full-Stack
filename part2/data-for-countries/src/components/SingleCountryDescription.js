3f43fd13935ca3f182318cd7dafbce5882a89f5f
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-05-22 20:35:42 -0500

    Used weather capital city component to separate concerns about the api

--- part2/data-for-countries/src/components/SingleCountryDescription.js
+++ part2/data-for-countries/src/components/SingleCountryDescription.js
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import WeatherCapitalCity from './WeatherCapitalCity'
 
 
 const SingleCountryDescription = ({countrySelected}) => {
-    const [capitalCity, setCapitalCity] = useState("");
-
-    // console.log(process.env.REACT_APP_KEY_WEATHER);
-    const falseKey = "e04abde689b39e92156ed89e6dea89f1"
-    
-    useEffect(() => {
-
-    }, [])
-
-  fetch(`http://api.weatherstack.com/current?access_key=${falseKey}`).then(resp=>console.log(resp.json()))
-
     const styleOfImg = {
         maxWidth: 150, 
         maxHeight: 150
     }
+    
 
     return (
         <div>
@@ -27,12 +18,9 @@ const SingleCountryDescription = ({countrySelected}) => {
         <ul>
             {countrySelected.languages.map(language => <li key={language.name}>{language.name}</li>)}
         </ul>
-        {<img alt={"flag of the country selected"} style={styleOfImg} src= {countrySelected.flag}></img>}
+        <img alt={`Flag of ${countrySelected.name}`} style={styleOfImg} src={countrySelected.flag}></img>
 
-        <h1>Weather in {countrySelected.capital}</h1>
-        <p><b>Temperature: </b> 232</p>
-        <img></img>
-        <p><b>Wind: </b> 232</p>
+        <WeatherCapitalCity capitalCity={countrySelected.capital} imgStyle={styleOfImg}/>
     </div>
     )
 }
--- part2/data-for-countries/src/components/WeatherCapitalCity.js
+++ part2/data-for-countries/src/components/WeatherCapitalCity.js
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from 'react'
 
 
-const WeatherCapitalCity = ({countryName}) => {
-    return (
-        <>
-        </>
-    )
+const WeatherCapitalCity = ({capitalCity, imgStyle}) => {
+    const [capitalWeatherObject, setCapitalWeatherObject] = useState(false);
+
+    //if the dependency array it's not the empty array, will be loaded every second (or more)
+    useEffect(() => {
+        fetch(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_KEY_WEATHER}&query=${capitalCity}`)
+        .then(resp => resp.json())
+        .then(weatherObject => {
+            setCapitalWeatherObject(weatherObject)
+        })
+    }, [])
+
+    //this will return the full object only after the object is loaded
+    if (capitalWeatherObject !== false) {
+        return (
+            <>
+                <h1>Weather in {capitalCity}</h1>
+                <p><b>Temperature: </b> {capitalWeatherObject.current.temperature} celcius.</p>
+                <img alt={capitalWeatherObject.current.weather_descriptions[0] } style={imgStyle} src={capitalWeatherObject.current.weather_icons[0]}></img>
+                <p><b>Wind: </b> {capitalWeatherObject.current.wind_speed} mph direction {capitalWeatherObject.current.wind_dir}</p>
+            </>
+        )
+    }
+
+    //If it's false, it's just the placeholder
+    else {
+        return (
+            <p>
+                Loading weather data...
+            </p>
+        )
+    }
 }
 export default WeatherCapitalCity
