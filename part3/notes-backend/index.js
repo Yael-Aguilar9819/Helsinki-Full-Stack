060a76be7f03984cec564b9beddee74a464db592
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-06-26 17:43:34 -0500

    Added more tests to the REST client and modified index.js to remove now unused comments

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -20,27 +20,6 @@ app.use(requestLogger)
 const mongoose = require('mongoose')
 const Note = require('./models/note.js')
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2019-05-30T17:30:31.098Z",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2019-05-30T18:39:34.091Z",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2019-05-30T19:20:14.298Z",
-//     important: true
-//   }
-// ]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!!</h1>');
 }) 
@@ -52,22 +31,13 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
+  }).catch(err => {
     response.status(404).end()
-  }
+  })
 })
 
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
@@ -83,7 +53,6 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    // id: generateId(),
   })
 
   newNote.save().then(savedNote => {
