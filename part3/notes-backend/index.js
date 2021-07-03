4968b575636bcfcf60fadad7d1f18f9820d5f700
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-03 00:19:20 -0500

    part 3D before pushing to heroku

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -42,25 +42,20 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
   console.log(body.content)
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
   const newNote = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important || false, 
     date: new Date(),
   })
 
-  newNote.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  newNote.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => response.json(savedAndFormattedNote)) 
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -99,7 +94,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
