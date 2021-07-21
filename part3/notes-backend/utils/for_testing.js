70bafe3c083799648d2cccf61b503a6092dbc332
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-20 23:28:20 -0500

    Added average.test and also corrected style with lint:fix

--- part3/notes-backend/utils/for_testing.js
+++ part3/notes-backend/utils/for_testing.js
@@ -1,19 +1,18 @@
-const palindrome = (string) => {
-    return string
-      .split('')
-      .reverse()
-      .join('')
-  }
-  
-  const average = (array) => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-  
-    return array.reduce(reducer, 0) / array.length
-  }
-  
-  module.exports = {
-    palindrome,
-    average,
-  }
+const palindrome = (string) => string
+  .split('')
+  .reverse()
+  .join('');
+
+const average = (array) => {
+  const reducer = (sum, item) => sum + item;
+
+  // If its not 0, will go correctly, otherwise will return 0
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length;
+};
+
+module.exports = {
+  palindrome,
+  average,
+};
