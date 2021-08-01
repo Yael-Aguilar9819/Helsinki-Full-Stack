f812c18ca5a71389e848e8c16e121807c7d769d2
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-08-01 05:31:29 -0500

    Removed braces in config so it's now working as it should

--- part3/notes-backend/utils/config.js
+++ part3/notes-backend/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-
-const { PORT } = process.env;
-const { MONGODB_URI } = process.env.NODE_ENV === 'test' 
+// removed braces, the destructuring method, these provoked a problem within the files
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
--- part3/notes-backend/utils/middleware.js
+++ part3/notes-backend/utils/middleware.js
@@ -1,11 +1,11 @@
 const logger = require('./logger');
+const config = require('./config');
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method);
   logger.info('Path:  ', request.path);
   logger.info('Body:  ', request.body);
   logger.info('---');
-  logger.info(process.env.NODE_ENV)
   next();
 };
 
