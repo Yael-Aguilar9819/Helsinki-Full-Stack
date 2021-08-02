93a38d6bc9a128034eb3acb4716bd61f78228859
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-08-02 07:08:18 -0500

    Added test to create and decreate the testDB, and corrected style

--- part3/notes-backend/tests/note_api.test.js
+++ part3/notes-backend/tests/note_api.test.js
@@ -1,28 +1,50 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
+
+const Note = require('../models/note');
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+];
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect('Content-Type', /application\/json/);
+});
 
 test('there are two notes', async () => {
-  const response = await api.get('/api/notes')
+  const response = await api.get('/api/notes');
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable response
-  expect(response.body).toHaveLength(2)
-})
+  expect(response.body).toHaveLength(2);
+});
 
 test('the first note is about HTTP methods', async () => {
-  const response = await api.get('/api/notes')
+  const response = await api.get('/api/notes');
 
-  expect(response.body[0].content).toBe('HTML is easy')
-})
+  expect(response.body[0].content).toBe('HTML is easy');
+});
 afterAll(() => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
--- part3/notes-backend/utils/config.js
+++ part3/notes-backend/utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 // removed braces, the destructuring method, these provoked a problem within the files
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === 'test' 
+const { PORT } = process.env;
+const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
+  : process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
