005ba3e47e1ffb1acff4ea273538c45f940207b9
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-08-25 03:03:31 -0500

    Reorganized app.js in notes-backend so it now more clearly separated

--- part3/notes-backend/app.js
+++ part3/notes-backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const config = require('./utils/config');
 require('express-async-errors');
 
 const app = express();
-const cors = require('cors');
 
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
@@ -11,7 +13,6 @@ const loginRouter = require('./controllers/login');
 
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
-const mongoose = require('mongoose');
 
 logger.info('connecting to', config.MONGODB_URI);
 
