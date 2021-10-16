14b1f91cd1a9cb68b661956901fb356e06fbb634
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-10-15 23:56:24 -0500

    Now handles logins correctly

--- part2/frontend/src/App.js
+++ part2/frontend/src/App.js
@@ -44,24 +44,24 @@ const App = () => {
     setNewNote("")
   }
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  
   }
-  try {
-    const user = await loginService.login({
-      username, password,
-    })
-    setUser(user)
-    setUsername('')
-    setPassword('')
-  } catch (exception) {
-    setErrorMessage('Wrong credentials')
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 5000)
-  }
-
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id)
