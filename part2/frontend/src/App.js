701fe5652219f389b07fb3e290df47276b29824d
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-10-15 23:42:00 -0500

    Added a way to connect the login segment to the backend through services/login

--- part2/frontend/src/App.js
+++ part2/frontend/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService.getAll()
@@ -46,6 +47,20 @@ const App = () => {
     event.preventDefault()
     console.log('logging in with', username, password)
   }
+  try {
+    const user = await loginService.login({
+      username, password,
+    })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  } catch (exception) {
+    setErrorMessage('Wrong credentials')
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id)
